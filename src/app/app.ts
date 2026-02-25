cc8d8c6814ee12251d66f8296e69b15fac8ca7d5
nbozinovic23 <nemanja.bozinovic.23@singimail.rs>
2026-02-25 16:27:19 +0100

    Created login component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
-    RouterLinkWithHref, 
-    MatButtonModule, 
+    RouterOutlet,
+    RouterLinkWithHref,
+    MatButtonModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
+
 }
